2229c17688528944ca69e904c74861cff375dee7
Wess58 <wesleymbate@gmail.com>
2018-08-29 21:54:04 +0300

    Added responsiveness for Mobile Devices

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,12 @@ $(".hover").mouseleave(
     $(this).removeClass("hover");
   }
 );
+//for Mobile Devices' screen size//
+$(function () {
+  var nua = navigator.userAgent
+  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+  if (isAndroid) {
+    $('select.form-control').removeClass('form-control').css('width', '100%')
+  }
+})
+</script>
